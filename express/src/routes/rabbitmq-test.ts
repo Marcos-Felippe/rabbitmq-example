5707b36917d892440f66d3ff63d5d52e0d659129
Marcos felippe <tsmfelippe@hotmail.com>
2022-07-13 14:31:37 -0300

    adding new keys commit

--- express/src/routes/rabbitmq-test.ts
+++ express/src/routes/rabbitmq-test.ts
@@ -7,7 +7,7 @@ router.post('/express', async function(req, res, next) {
   const server = new RabbitmqServer('amqp://admin:admin@localhost:5672');
   await server.start();
   //await server.publishInQueue('nest', JSON.stringify(req.body));
-  await server.publishInExchange('amq.direct', 'rota1', JSON.stringify(req.body));
+  await server.publishInExchange('amq.direct', 'ne', JSON.stringify(req.body));
   res.send(req.body);
 });
 
